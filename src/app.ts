8045c0fd168e37a59b181ca075087cfaac37fbeb
lebrancconvas <lebrancconvas@gmail.com>
2022-05-15 02:32:30 +0700

    🔨 Create Automation Function.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-console.log("BEGIN SUCCESS.");
+import puppeteer from 'puppeteer';
+
+const app = async() => {
+	const browser = await puppeteer.launch({headless: true});
+	const page = await browser.newPage();
+
+	page.goto('https://www.ddproperty.com/นโยบายความเป็นส่วนตัว');
+	await page.waitForTimeout(2000);
+
+	const selector = "article.article-detail";
+
+	await page.waitForSelector(".article-detail");
+
+	const element = await page.$(selector);
+
+	const value = await page.evaluate(el => el.textContent, element);
+
+	await page.close();
+	await browser.close();
+
+	console.log(value);
+};
+
+app();

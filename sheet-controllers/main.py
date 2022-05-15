049528f48c23daec07e15f33b5f351cb7925bed4
lebrancconvas <lebrancconvas@gmail.com>
2022-05-16 04:09:19 +0700

    🔨 Text Reading.

--- sheet-controllers/main.py
+++ sheet-controllers/main.py
@@ -2,13 +2,26 @@ from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 
 
-scope = ['https://www.googleapis.com/auth/spreadsheets.readonly'] 
+def get_sheet_data():
+	scope = ['https://www.googleapis.com/auth/spreadsheets'] 
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-gc = gspread.authorize(credentials)
+	credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+	gc = gspread.authorize(credentials)
 
-sheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/15eJoqC8c6k1BkMXMU2epuuIE9dt4JblwRX96KgfNF0A/edit?usp=sharing")
+	sheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/15eJoqC8c6k1BkMXMU2epuuIE9dt4JblwRX96KgfNF0A/edit?usp=sharing")
 
-worksheet = sheet.get_worksheet(0)
+	worksheet = sheet.get_worksheet(0)
 
-print(worksheet.get_all_values()) 
+	print(worksheet.get_all_values()) 
+ 
+def get_text_data():
+  with open('../automation/data/output.txt') as f:
+    contents = f.read()
+    return contents
+
+
+def data_management():
+  text_data = get_text_data()
+  print(text_data)
+
+data_management()
